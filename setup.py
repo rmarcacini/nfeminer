b5533f9eee43cd9b4bc6d76541e7f31ce1a1e710
rmarcacini <40037976+rmarcacini@users.noreply.github.com>
2023-03-10 11:56:26 -0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    name='nfeminer',
+    version='0.1.0',    
+    description='NFeMiner package',
+    url='https://github.com/rmarcacini/nfeminer',
+    author='Ricardo Marcacini',
+    author_email='ricardo.marcacini@usp.br',
+    license='MIT',
+    packages=['nfeminer'],
+    install_requires=[],
+
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Science/Research',
+        'Operating System :: POSIX :: Linux',        
+        'Programming Language :: Python :: 3'
+    ],
+)
